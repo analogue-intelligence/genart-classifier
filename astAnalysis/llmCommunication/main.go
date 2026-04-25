2cacbc0bc5dab3eda9dc56cd323d1f02d21ad6c4
Mauricio Verano Merino <m.verano239@gmail.com>
2026-04-25 13:26:04 +0200

    added results for the few shot examples

--- astAnalysis/llmCommunication/main.go
+++ astAnalysis/llmCommunication/main.go
@@ -9,8 +9,31 @@ import (
 	"github.com/firebase/genkit/go/ai"
 	"github.com/firebase/genkit/go/genkit"
 	"github.com/firebase/genkit/go/plugins/compat_oai"
+
 	"github.com/joho/godotenv"
+
+	"bytes"
+	_ "embed"
+	"text/template"
 )
+
+//go:embed prompts/baseline/original_prompt.txt
+var baselineTemplate string
+
+//go:embed prompts/icl/original_prompt_few.txt
+var baselineFewTemplate string
+
+var exampleProgram = `
+function setup() {
+  createCanvas(400, 400);
+}
+function draw() {
+  background(220);
+  let d = dist(mouseX, mouseY, width/2, height/2);
+  fill(d, 100, 255);
+  ellipse(width/2, height/2, d);
+}
+`
 
 var systemDefinition = "You are an expert evaluator of software-based artworks."
 
@@ -45,14 +68,30 @@ func main() {
 	}
 
 	resp, err := genkit.Generate(ctx, g,
-		ai.WithModelName("nebula/deepseek-r1:1.5b"),
+		ai.WithModelName("nebula/deepseek-r1:8b"),
 		ai.WithSystem(systemDefinition),
-		ai.WithPrompt("What is the capital of Brazil?"),
+		ai.WithPrompt(ExecutePrompt(exampleProgram)),
 		ai.WithOutputSchema(outputSchema),
+		ai.WithConfig(map[string]any{
+			"Temperature": 0.0,
+		}),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Printf("Response: %s\n\n", resp.Text())
+}
+
+func ExecutePrompt(artwork string) string {
+	tmpl, _ := template.New("prompt").Parse(baselineTemplate)
+
+	var buf bytes.Buffer
+	data := map[string]interface{}{
+		"Artwork":   artwork,
+		"Extension": ".js",
+		"File_name": "bla",
+	}
+	tmpl.Execute(&buf, data)
+	return buf.String()
 }
